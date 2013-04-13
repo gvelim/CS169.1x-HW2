a7a02db8c8a936f6a26bd3df0630c79581d0491b
saasbook <saasbook@saasbook.(none)>
2013-04-14 00:22:04 +0300

    fixed redirect

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,26 +8,41 @@ class MoviesController < ApplicationController
 
   def index
     
-    # load cache structure with keys -> false
-    @all_ratings = Movie.ratings_cache
-    
-    # if filter data sent with GET extract them otherwise
-      # if first time filter set all visible via @all_ratings.keys otherwise pull from session
-    @filter = params.has_key?(:filter) ? params[:filter] : (session[:filter] || @all_ratings.keys) 
-    
-    # if any rating changes set relevant keys -> true
-    if params.has_key? :commit then
-      if params.has_key? :ratings then 
-        session[:filter] = @filter = params[:ratings].keys
-       else
-        redirect_to movies_path, :filter => @filter
-      end
-    end
-
     # if order GET sent then update sesssion and pass to @orderby
       # otherwise pass the session (either nil or anyval)
     @orderby = params.has_key?(:orderby) ? (session[:orderby] = params[:orderby]) : session[:orderby] 
-
+    
+    # load cache structure with keys -> false
+    @all_ratings = Movie.ratings_cache
+        
+    # if any rating changes set relevant keys -> true
+    if params.has_key? :commit then
+      if params.has_key? :ratings then 
+        @filter = session[:filter] = params[:ratings].keys
+      else
+        flash.keep
+        redirect_to :filter => @filter, :orderby => @orderby
+        return
+      end
+    else
+          
+      # if filter data sent with GET extract them otherwise
+        # if first time filter set all visible via @all_ratings.keys otherwise pull from session
+      if !params.has_key? :filter  then
+        if session[:filter] then
+          @filter = session[:filter] 
+          flash.keep
+          redirect_to :filter => @filter, :orderby => @orderby
+          return
+        else
+          @filter = session[:filter] = @all_ratings.keys 
+        end
+      else
+        @filter = params[:filter]
+      end  
+    
+    end
+    
     # filter Movie with :rating, :order    
     @movies = Movie.where( :rating => @filter ).order( @orderby ).all
 
@@ -40,7 +55,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path, :orderby => session[:orderby], :filter => session[:filter]
+    redirect_to movies_path
   end
 
   def edit
@@ -51,14 +66,14 @@ class MoviesController < ApplicationController
     @movie = Movie.find params[:id]
     @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie), :orderby => session[:orderby], :filter => session[:filter]
+    redirect_to movie_path(@movie)
   end
 
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
-    redirect_to movies_path, :orderby => session[:orderby], :filter => session[:filter]
+    redirect_to movies_path
   end
 
 end
